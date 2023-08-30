32577b16c1f65769a47a8916a0b2de0483b85746
Mahmoud Qandeel <bb.qandeeil@gmail.com>
2023-08-30 21:24:49 +0300

    Admin protection

--- src/components/Registration/SignUp/SectionRight/Home.js
+++ src/components/Registration/SignUp/SectionRight/Home.js
@@ -1,52 +1,78 @@
-import React from 'react'
-import '../../../../styles/Registration/SignUp/SectionRight/Home.scss'
-import polygon1 from './icon/Polygon1.svg'
-import polygon2 from './icon/Polygon2.svg'
-import user from './icon/user.svg'
-import briefcase from './icon/briefcase.svg'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from "react";
+import "../../../../styles/Registration/SignUp/SectionRight/Home.scss";
+import polygon1 from "./icon/Polygon1.svg";
+import polygon2 from "./icon/Polygon2.svg";
+import user from "./icon/user.svg";
+import briefcase from "./icon/briefcase.svg";
+import { NavLink } from "react-router-dom";
 
-const SectionRight = ({setHome, setPersonalInfo, setCaseSignup}) => {
-    const clickHandler = (e) => {
-        setHome(false)
-        setPersonalInfo(true)
-        setCaseSignup(e)
+const SectionRight = ({ setHome, setPersonalInfo, setCaseSignup }) => {
+  const clickHandler = (e) => {
+    setErrorCode(true);
+    if (code == "2914") {
+      setErrorCode(false);
+      setHome(false);
+      setPersonalInfo(true);
+      setCaseSignup(e);
     }
+  };
+  const [showCode, setShowCode] = useState(false);
+  const [code, setCode] = useState();
+  const [errorCode, setErrorCode] = useState(false);
   return (
-    <div className='homeSectionRight'>
-        <div className='headerSignIn'>
-            <span>Already have an account? <NavLink to={'/Signin'}>Sign In</NavLink></span>
+    <div className="homeSectionRight">
+      <div className="headerSignIn">
+        <span>
+          Already have an account? <NavLink to={"/Signin"}>Sign In</NavLink>
+        </span>
+      </div>
+      <div className="selectForm">
+        <div className="header">
+          <h1>Join Us!</h1>
+          <span>
+            To begin this journey, tell us what type of account you’d be
+            opening.
+          </span>
         </div>
-        <div className='selectForm'>
-            <div className='header'>
-                <h1>Join Us!</h1>
-                <span>To begin this journey, tell us what type of account you’d be opening.</span>
-            </div>
-            <div className='box' onClick={() => clickHandler('user')}>
-                <div className='icon'>
-                    <img src={polygon1}/>
-                    <img src={user}/>
-                </div>
-                <div className='contentBox'>
-                    <h1>Individual</h1>
-                    <p>Personal account to manage all you activities.</p>
-                </div>
-                <div className='backgroundIcon'></div>
-            </div>
-            <div className='box' onClick={() => clickHandler('admin')}>
-                <div className='icon'>
-                    <img src={polygon2}/>
-                    <img src={briefcase}/>
-                </div>
-                <div className='contentBox'>
-                    <h1>Business</h1>
-                    <p>Own or belong to a company, this is for you.</p>
-                </div>
-                <div className='backgroundIcon'></div>
-            </div>
+        <div className="box" onClick={() => clickHandler("user")}>
+          <div className="icon">
+            <img src={polygon1} />
+            <img src={user} />
+          </div>
+          <div className="contentBox">
+            <h1>Individual</h1>
+            <p>Personal account to manage all you activities.</p>
+          </div>
+          <div className="backgroundIcon"></div>
         </div>
+        <div className="box" onClick={() => setShowCode(true)}>
+          <div className="icon">
+            <img src={polygon2} />
+            <img src={briefcase} />
+          </div>
+          <div className="contentBox">
+            <h1>Business</h1>
+            <p>Own or belong to a company, this is for you.</p>
+          </div>
+          <div className="backgroundIcon"></div>
+        </div>
+      </div>
+      {showCode && (
+        <div className="enterCode">
+          <div className="code">
+            <h1>Add the key to continue</h1>
+            <input
+              type="password"
+              placeholder="Enter the code"
+              onChange={(e) => setCode(e.target.value)}
+              className={errorCode && 'errorInput'}
+            />
+            <button onClick={() => clickHandler("admin")}>Check code</button>
+          </div>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default SectionRight
+export default SectionRight;
