7d6b8deb3748873e1dc196fd655b7840b37712d8
Mahmoud Qandeel <bb.qandeeil@gmail.com>
2023-09-01 00:31:38 +0300

    update window location

--- src/screens/Registration/SignUp.tsx
+++ src/screens/Registration/SignUp.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../../styles/Registration/SignUp/SignUp.scss";
 import SectionLeft from "../../components/Registration/SectionLeft/SectionLeft";
 import Home from "../../components/Registration/SignUp/SectionRight/Home";
 import PersonalInfo from "../../components/Registration/SignUp/SectionRight/PersonalInfo";
 import ResidencyInfo from "../../components/Registration/SignUp/SectionRight/ResidencyInfo";
+import useLocalStorage from "use-local-storage";
 
 type Props = {};
 
@@ -12,6 +13,13 @@ const SignUp: React.FC<Props> = () => {
   const [personalInfo, setPersonalInfo] = useState(false);
   const [residencyInfo, setResidencyInfo] = useState(false);
   const [caseSignup, setCaseSignup] = useState();
+
+  const [dataAccount,] = useLocalStorage("DataAccount", null);
+  useEffect(() => {
+    if (dataAccount) {
+      window.location.href = "/signin";
+    }
+  }, [dataAccount]);
 
   return (
     <div className="signUp">
